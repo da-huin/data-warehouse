be17159f0e77171bc11b8fce516aef4c632858cd
Park <dahuin000@gmail.com>
2020-08-10 02:01:04 +0900

    function docstring added

--- data_warehouse/__init__.py
+++ data_warehouse/__init__.py
@@ -11,21 +11,40 @@ import time
 import easy_s3
 
 class DataWarehouse():
+    """
+    데이터 웨어하우스를 만들 때 사용하는 간소화된 인터페이스입니다.
 
+    Parameters
+    ----------
+    
+    * bucket_name: str
+
+        데이터 웨어하우스에 사용할 버킷 이름
+
+    * table_name: str
+
+        테이블 이름
+
+    * aws_access_key_id: str
+        
+        AWS ACCESS KEY ID
+
+    * aws_secret_access_key: str
+        
+        AWS SECRET ACCESS KEY
+
+    * region_name: str
+        
+        AWS REGION NAME
+
+    * warehouse_name: str
+        
+        웨어하우스 이름입니다. 
+
+        default/[warehouse_name]/... 와 같은 경로로 저장됩니다.
+    
+    """
     def __init__(self, bucket_name, table_name="", aws_access_key_id=None, aws_secret_access_key=None, region_name=None, warehouse_name="warehouse"):
-        """
-        1. 저장
-            * raw_save
-            * discovery_save
-        2. 불러오기
-            * raw_load
-            * discovery_load
-        3. 리스트 불러오기
-            * raw_list
-            * discover_list
-        4. 아테나로 데이터 불러오기
-            * load_with_athena_query
-        """
 
         self.warehouse_name = warehouse_name
         self.bucket_name = bucket_name
@@ -41,7 +60,49 @@ class DataWarehouse():
     # 실제 사용하는 기능들
     def raw_save(self, user_path, value, meta={}, use_ymd=True, table_name="", table_kind="general", ymd=""):
         """
-        raw 저장소에 저장합니다.
+        * 만약 parquet 확장자로 저장하려면 `fastparquet` 와 `pandas` 를 설치해야 합니다.
+
+        * 기본적으로 gz 압축되어 저장됩니다.
+
+        `RAW 저장되는 경로`: raw_save 는 비정형 데이터를 저장 할 때 사용합니다. 저장되는 경로는 아래와 같습니다.
+
+            default/warehouse/raw/[table_kind]/[table_name]/[ymd]/[user_path]
+
+        **Parameters**
+
+        * `(required) user_path`: str
+
+            위의 `저장되는 경로` 를 참조하세요.
+
+        * `(required) value`: dict | list | str | bytes | int | float | ...
+
+            저장 할 데이터입니다.
+
+        * `meta`: dict (default = {})
+
+            저장한 시간 등 기본 메타데이터가 저장되는데 추가로 저장 할 메타데이터입니다.
+
+        * `use_ymd`: bool (default = True)
+
+            위의 `저장되는 경로` 에서 ymd 부분을 자동으로 오늘 날짜로 지정 할 지 결정합니다. False 라면 ymd 폴더가 생성되지 않습니다.
+
+        * `table_name`: str (default = "")
+
+            테이블 이름입니다. 만약 핸들러를 만들 때 table_name 을 넣어주었다면 생략해도 됩니다.
+
+        * `table_kind`: str (default = "general")
+
+            테이블 분류입니다. 특별한 목적이 없다면 기본값으로 두는 편이 좋습니다.
+
+        * `ymd`: str (default = "")
+            
+            use_ymd 는 현재 시간으로 저장되는데, 현재 시간이 아닌 다른 시간으로 저장 할 때 사용하는 매개변수입니다.
+            
+            이 매개변수를 2020-08-30 과 같은 형식으로 입력하면 `저장되는 경로` 에서 ymd가 2020-08-30 으로 바뀝니다.
+
+        **Returns**
+
+        * `S3에 저장 된 파일의 URI`: str
         """
         return self._save_worker("raw", table_kind, user_path,
                                  value, meta, use_ymd, table_name, ymd)
@@ -49,41 +110,245 @@ class DataWarehouse():
 
     def discovery_save(self, user_path, value, meta={}, use_ymd=True, table_name="", table_kind="general", ymd=""):
         """
-        discovery 저장소에 저장합니다.
+        * 만약 parquet 확장자로 저장하려면 `fastparquet` 와 `pandas` 를 설치해야 합니다.
+
+        * 기본적으로 gz 압축되어 저장됩니다.
+
+        `DISCOVERY 저장되는 경로`: discovery_save 는 반정형, 정형 데이터를 저장 할 때 사용합니다. 저장되는 경로는 아래와 같습니다.
+
+            default/warehouse/discovery/[table_kind]/[table_name]/[ymd]/[user_path]
+
+        **Parameters**
+
+        * `(required) user_path`: str
+
+            위의 `저장되는 경로` 를 참조하세요.
+
+        * `(required) value`: dict | list | str | bytes | int | float | ...
+
+            저장 할 데이터입니다.
+
+        * `meta`: dict (default = {})
+
+            저장한 시간 등 기본 메타데이터가 저장되는데 추가로 저장 할 메타데이터입니다.
+
+        * `use_ymd`: bool (default = True)
+
+            위의 `저장되는 경로` 에서 ymd 부분을 자동으로 오늘 날짜로 지정 할 지 결정합니다. False 라면 ymd 폴더가 생성되지 않습니다.
+
+        * `table_name`: str (default = "")
+
+            테이블 이름입니다. 만약 핸들러를 만들 때 table_name 을 넣어주었다면 생략해도 됩니다.
+
+        * `table_kind`: str (default = "general")
+
+            테이블 분류입니다. 특별한 목적이 없다면 기본값으로 두는 편이 좋습니다.
+
+        * `ymd`: str (default = "")
+            
+            use_ymd 는 현재 시간으로 저장되는데, 현재 시간이 아닌 다른 시간으로 저장 할 때 사용하는 매개변수입니다.
+            
+            이 매개변수를 2020-08-30 과 같은 형식으로 입력하면 `저장되는 경로` 에서 ymd가 2020-08-30 으로 바뀝니다.
+
+        **Returns**
+
+        * `S3에 저장 된 파일의 URI`: str
         """           
         return self._save_worker("discovery", table_kind, user_path,
                                  value, meta, use_ymd, table_name, ymd)
 
     def raw_load(self, user_path, table_name="", ymd="", is_meta=False, table_kind="general"):
         """
-        raw 저장소에서 불러옵니다.
+        `RAW 불러오는 경로`: raw_load 는 비정형 데이터를 불러 올 때 사용합니다. 불러오는 경로는 아래와 같습니다.
+
+            default/warehouse/raw/[table_kind]/[table_name]/[ymd]/[user_path]
+
+        **Parameters**
+
+        * `(required) user_path`: str
+
+            위의 `불러오는 경로` 를 참조하세요.
+
+        * `table_name`: str (default = "")
+
+            테이블 이름입니다. 만약 핸들러를 만들 때 table_name 을 넣어주었다면 생략해도 됩니다.
+
+        * `table_kind`: str (default = "general")
+
+            테이블 분류입니다. 특별한 목적이 없다면 기본값으로 두는 편이 좋습니다.
+
+        * `ymd`: str (default = "")
+            
+            이 매개변수를 2020-08-30 과 같은 형식으로 입력하면 `불러오는 경로` 에서 ymd가 2020-08-30 으로 바뀝니다.
+
+        * `is_meta`: bool (default = False)
+
+            메타데이터를 불러 올 지 데이터를 불러 올 지 설정하는 매개변수입니다.
+
+
+        **Returns**
+
+        * `불러온 데이터`: str | dict | list | ...
         """             
         return self._load_worker("raw", table_name, user_path, table_kind, ymd, is_meta)
 
 
     def discovery_load(self, user_path, table_name="", ymd="", is_meta=False, table_kind="general"):
         """
-        discovery 저장소에서 불러옵니다.
+        `DISCOVERY 불러오는 경로`: discovery_load 는 반정형, 정형 데이터를 불러 올 때 사용합니다. 불러오는 경로는 아래와 같습니다.
+
+            default/warehouse/discovery/[table_kind]/[table_name]/[ymd]/[user_path]
+
+        **Parameters**
+
+        * `(required) user_path`: str
+
+            위의 `불러오는 경로` 를 참조하세요.
+
+        * `table_name`: str (default = "")
+
+            테이블 이름입니다. 만약 핸들러를 만들 때 table_name 을 넣어주었다면 생략해도 됩니다.
+
+        * `table_kind`: str (default = "general")
+
+            테이블 분류입니다. 특별한 목적이 없다면 기본값으로 두는 편이 좋습니다.
+
+        * `ymd`: str (default = "")
+            
+            이 매개변수를 2020-08-30 과 같은 형식으로 입력하면 `불러오는 경로` 에서 ymd가 2020-08-30 으로 바뀝니다.
+
+        * `is_meta`: bool (default = False)
+
+            메타데이터를 불러 올 지 데이터를 불러 올 지 설정하는 매개변수입니다.
+
+
+        **Returns**
+
+        * `불러온 데이터`: str | dict | list | ...
         """          
         return self._load_worker("discovery", table_name, user_path, table_kind, ymd, is_meta)
         
 
     def raw_list(self, table_name="", user_dir="", ymd="", table_kind="general", load=False, is_meta=False, includes=[], is_direoctry=False):
         """
-        raw 저장소에서 리스트합니다.
+        `RAW 리스트하는 경로`: raw_load 는 비정형 데이터를 리스트 할 때 사용합니다. 리스트하는 경로는 아래와 같습니다.
+
+            default/warehouse/raw/[table_kind]/[table_name]/[ymd]/[user_dir]
+
+        **Parameters**
+
+        * `(required) user_dir`: str
+
+            위의 `리스트하는 경로` 를 참조하세요.
+
+        * `table_name`: str (default = "")
+
+            테이블 이름입니다. 만약 핸들러를 만들 때 table_name 을 넣어주었다면 생략해도 됩니다.
+
+        * `table_kind`: str (default = "general")
+
+            테이블 분류입니다. 특별한 목적이 없다면 기본값으로 두는 편이 좋습니다.
+
+        * `ymd`: str (default = "")
+            
+            이 매개변수를 2020-08-30 과 같은 형식으로 입력하면 `리스트하는 경로` 에서 ymd가 2020-08-30 으로 바뀝니다.
+
+        * `is_meta`: bool (default = False)
+
+            메타데이터를 리스트 할 지 데이터를 리스트 할 지 설정하는 매개변수입니다.
+
+        * `load` bool (default = False)
+
+            리스트하며 데이터를 불러올 수 있습니다.
+
+        * `is_direoctry`: bool (default = False)
+
+            파일이 아닌 디렉토리를 리스트 할 수 있습니다.
+
+        * `includes`: str list 
+
+            리스트를 이 매개변수에 포함된 값으로 필터합니다.
+
+
+        **Returns**
+
+        * `리스트 결과`: list
         """          
         return self._list_worker("raw", table_kind, table_name, user_dir, ymd, load, is_meta, includes, is_direoctry)
 
     def discovery_list(self, table_name="", user_dir="", ymd="", table_kind="general", load=False, is_meta=False, includes=[], is_direoctry=False):
         """
-        discovery 저장소에서 리스트합니다.
+        `DISCOVERY 리스트하는 경로`: discovery_load 는 반정형, 정형 데이터를 리스트 할 때 사용합니다. 리스트하는 경로는 아래와 같습니다.
+
+            default/warehouse/discovery/[table_kind]/[table_name]/[ymd]/[user_dir]
+
+        **Parameters**
+
+        * `(required) user_dir`: str
+
+            위의 `리스트하는 경로` 를 참조하세요.
+
+        * `table_name`: str (default = "")
+
+            테이블 이름입니다. 만약 핸들러를 만들 때 table_name 을 넣어주었다면 생략해도 됩니다.
+
+        * `table_kind`: str (default = "general")
+
+            테이블 분류입니다. 특별한 목적이 없다면 기본값으로 두는 편이 좋습니다.
+
+        * `ymd`: str (default = "")
+            
+            이 매개변수를 2020-08-30 과 같은 형식으로 입력하면 `리스트하는 경로` 에서 ymd가 2020-08-30 으로 바뀝니다.
+
+        * `is_meta`: bool (default = False)
+
+            메타데이터를 리스트 할 지 데이터를 리스트 할 지 설정하는 매개변수입니다.
+
+        * `load` bool (default = False)
+
+            리스트하며 데이터를 불러올 수 있습니다.
+
+        * `is_direoctry`: bool (default = False)
+
+            파일이 아닌 디렉토리를 리스트 할 수 있습니다.
+
+        * `includes`: str list 
+
+            리스트를 이 매개변수에 포함된 값으로 필터합니다.
+
+
+        **Returns**
+
+        * `리스트 결과`: list
         """           
         return self._list_worker("discovery", table_kind, table_name, user_dir, ymd, load, is_meta, includes, is_direoctry)
 
     def load_with_full_path(self, full_path):
+        """
+        S3 전체 경로로 데이터를 불러와야 할 때 사용하는 함수입니다.
+
+        **Parameters**
+
+        * `full_path`: str
+
+        **Returns**
+
+        * `불러온 데이터`: dict | list | str | ...        
+        """
         return self._es_handler._load_file(full_path)
 
     def load_meta_with_full_path(self, full_path):
+        """
+        S3 전체 경로로 메타 데이터를 불러와야 할 때 사용하는 함수입니다.
+
+        **Parameters**
+
+        * `full_path`: str
+
+        **Returns**
+
+        * `불러온 메타 데이터`: dict        
+        """
         return self._es_handler._load_file(self._get_meta_path_with_full_path(full_path))
 
     def _get_meta_path_with_full_path(self, full_path):
@@ -92,6 +357,26 @@ class DataWarehouse():
 
         return "/".join(splited)
     def load_with_athena_query(self, query, format_type="auto", request_limit=10000):
+        """
+        아테나로 데이터를 불러 올 때 사용합니다.
+
+        **Parameters**
+
+        * `query`: str
+
+            아테나로 요청할 쿼리입니다.
+
+            ```
+            SELECT * FROM dbname.tablename
+            ```
+
+        * `request_limit`: int (default=10000)
+
+
+        **Returns**
+
+        * `아테나 쿼리 결과`: list        
+        """
         def get_format_type(query):
             if query.find("SELECT") != -1 or query.find("select") != -1:
                 return "select"
